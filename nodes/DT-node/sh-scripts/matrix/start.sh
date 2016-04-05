cd26e57dbef8037344b6baf0267f0ee0a2ccdea2
Cloud-init-user <debian@dockerhost.novalocal>
2016-04-05 08:41:07 +0000

    minor fix in matrix start-script

--- nodes/DT-node/sh-scripts/matrix/start.sh
+++ nodes/DT-node/sh-scripts/matrix/start.sh
@@ -7,7 +7,7 @@ if [ $? -eq 0 ]; then
 	echo "matrix container already started ... "
 else 
 	echo "starting matrix container in a screen session ..." 
-	screen -d -m -S matrix /bin/sh -c "/opt/rethink/dev-msg-node-matrix/dist/docker/start.sh && /bin/sh"
+	screen -d -m -S matrix /bin/sh -c "/opt/rethink/dev-msg-node-matrix/dist/docker/start.sh && /bin/bash"
 	echo "The Matrix container is now running detached in background ---> starting a console for monitoring purposes!"
 fi
 
