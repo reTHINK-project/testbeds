1ccdc2ea78229d4fcd70428a18c20bd67a20a29d
Marc Emmelmann <marc.emmelmann@fokus.fraunhofer.de>
2016-06-13 14:23:24 +0200

    now having 200 independent replicas for experiment 1

--- dev/performance-tests/catalogue/tests/drawAllFigures.m
+++ dev/performance-tests/catalogue/tests/drawAllFigures.m
@@ -1,4 +1,4 @@
 
-generateFiguresMatLab('response_time_numcalls_1_', 112, 'NumConnections=20; NumCallsPerConnection=1')
+generateFiguresMatLab('response_time_numcalls_1_', 200, 'NumConnections=20; NumCallsPerConnection=1')
 %generateFiguresMatLab('response_time_numcalls_10_', 2, 'NumConnections=20; NumCallsPerConnection=10')
 
