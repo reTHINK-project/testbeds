b48d4d014dc64217c23ab31290f07886ab75989d
sbecot <simon.becot@orange.com>
2017-01-24 16:16:11 +0100

    update

--- dev-hello/clientBootStrap.js
+++ dev-hello/clientBootStrap.js
@@ -5,7 +5,7 @@
 
 /** Public region **/
 
-/** TODO --> Chix du domain via input **/
+/** TODO --> Choix du domain via input **/
 let domain;
 let bootStrapDomain = "call.rethink.orange-labs.fr";
 
@@ -14,84 +14,49 @@ let bootStrapDomain = "call.rethink.orange-labs.fr";
 let RUNTIME;
 let runtimeURL;
 
-
-/** Load Hyperty Runtime **/
-$(document).ready(function () {
-  askDomain();
-  //loadRuntime();
-});
-
-// waits for the rethink.js to be loaded.
-function checkLoading()
-{
-   if(typeof rethink === "undefined"){
-	setTimeout(function(){
-		checkLoading();
-	},250);
-    } else {
-		doLoading();
-	}
-}
-
-// Loads the runtime.
-function doLoading(){
-  var start = new Date().getTime();
-   rethink.default.install({
-    domain: domain,
-    development: false,
-    runtimeURL: runtimeURL
-  }).then((runtime) => {
-    RUNTIME = runtime
-    var time = (new Date().getTime()) - start;
-    var logs = $('.call-to-action');
-    logs.append('<span>Runtime has been successfully launched in ' + time / 1000 + ' seconds</sapn>');
-  });
-}
-/** Load Runtme reThink **/
-function loadRuntime() {
-  //Rethink runtime is included in index.html
-  checkLoading();
-}
-
-function removeModal(modalToClose) {
-  modalToClose.removeClass('show');
-}
-
-// evenement qui appelle la fonction removeModal()
-function removeModalHandler() {
-  var modal = $('#popup');
-  removeModal(modal);
-}
-
-
-function askDomain() {
-  var modal = $('#popup');
-  var close = $('.close');
-  modal.addClass('show');
-
-  close.click(function (event) {
-    event.stopPropagation();
-    removeModalHandler();
-  });
-
-}
-
+/**
+ returns a Promise. Success when runtime is loaded.
+*/
 function registerDomain() {
-  var currentDomain = $("#domainUri").val();
-  if (currentDomain != "") {
-    domain = currentDomain;
-    bootStrapDomain = currentDomain;
-    var my_awesome_script = document.createElement('script');
-    my_awesome_script.setAttribute('src',`https://${bootStrapDomain}/.well-known/runtime/rethink.js`);
-    document.head.appendChild(my_awesome_script);
-
-    runtimeURL = `hyperty-catalogue://catalogue.${domain}/.well-known/runtime/Runtime`; 
-    removeModalHandler();
-    loadRuntime();
-  }
-  else {
-    $(".need").show();
-  }
+	var currentDomain = bootStrapDomain;//$("#domainUri").val();
+	domain = currentDomain;
+	bootStrapDomain = currentDomain;
+	var promise = new Promise(function(resolve, reject) {
+		var script = document.createElement('script');
+        script.src = `https://${bootStrapDomain}/.well-known/runtime/rethink.js`;
+		script.addEventListener('load', function() {
+                resolve(script);
+            }, false);
+            script.addEventListener('error', function() {
+                reject(script);
+                console.log('was rej');
+            }, false);
+            document.head.appendChild(script);
+	});
+	return promise.then(
+	// rethink.js loaded
+	function(result){
+		// Promise to load the runtime
+		return new Promise(function(resolve, reject) {
+		  var start = new Date().getTime();
+		  runtimeURL = `hyperty-catalogue://catalogue.${domain}/.well-known/runtime/Runtime`; 
+		  rethink.default.install({
+			domain: domain,
+			development: false,
+			runtimeURL: runtimeURL
+		  }).then((runtime) => {
+				RUNTIME = runtime
+				var time = (new Date().getTime()) - start;
+				var logs = $('.call-to-action');
+				logs.append('<span>Runtime has been successfully launched in ' + time / 1000 + ' seconds</sapn>');
+				resolve("Success");
+			  }, 
+			  (err) => {
+					reject(Error("Unable to load the runtime at " + runtimeURL));
+				}
+			);
+		});
+	});
 }
 
 
