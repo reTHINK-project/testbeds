e8bf9ca698bf5a87ae4e3c29d2832d2fe4f10434
Steffen Druesedow <steffen.druesedow@telekom.de>
2017-03-16 08:08:18 +0000

    DT-Node: added proxy rule for umbrella app; added build entry for domain-registry; updated lets-encrypt script

--- nodes/DT-node/letsencrypt/letsencrypt-cron.sh
+++ nodes/DT-node/letsencrypt/letsencrypt-cron.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "Stopping Proxy Docker Container"
-/opt/rethink/testbeds/nodes/DT-node/sh-scripts/proxy/stop.sh
+docker-compose -f /opt/rethink/testbeds/nodes/DT-node/docker-compose/stable/docker-compose.yaml stop proxy
 
 echo "Starting letsencrypt-auto for domain"
 sudo /opt/letsencrypt/letsencrypt-auto renew -nvv --standalone 
 
-sudo p /etc/letsencrypt/live/rethink.tlabscloud.com/cert.pem /opt/rethink/testbeds/docker/apache2-reverse-proxy-dt/keys/server.crt
-sudo cp /etc/letsencrypt/live/rethink.tlabscloud.com/privkey.pem /opt/rethink/testbeds/docker/apache2-reverse-proxy-dt/keys/server.key
+sudo p /etc/letsencrypt/live/rethink.tlabscloud.com/cert.pem /opt/rethink/testbeds/docker/apache2-reverse-proxy-dt/keys/stable/server-stable.crt
+sudo cp /etc/letsencrypt/live/rethink.tlabscloud.com/privkey.pem /opt/rethink/testbeds/docker/apache2-reverse-proxy-dt/keys/server-stable.key
+sudo cp /etc/letsencrypt/live/rethink.tlabscloud.com/chain.pem /opt/rethink/testbeds/docker/apache2-reverse-proxy-dt/keys/chain.pem
 
 echo "Re-Starting Proxy Docker Container"
-/opt/rethink/testbeds/nodes/DT-node/sh-scripts/proxy/start.sh
+docker-compose -f /opt/rethink/testbeds/nodes/DT-node/docker-compose/stable/docker-compose.yaml start proxy
 
