05051f53ffb77094f36ffa8792d5621f7a59358c
pchainho <pchainho@gmail.com>
2017-07-14 00:21:02 +0100

    D6.4 update

--- docs/d6.4/d6.4-3-development.cmd
+++
@@ -1,5 +0,0 @@
-
-
-pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. https://raw.githubusercontent.com/reTHINK-project/hackathons/master/2017-internal/hyperty-survey-results/hyperty-survey-results.md -o D6.4-31-hyperty-development.docx
-
-pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. https://raw.githubusercontent.com/reTHINK-project/hackathons/master/2017-internal/dev-app-survey-results/app-survey-results.md -o D6.4-32-app-development.docx
