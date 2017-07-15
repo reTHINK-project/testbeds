01813175a1d27fdcb0b203581c2f7d0bce65e704
pchainho <pchainho@gmail.com>
2017-07-15 01:41:15 +0100

    D6.4 updated with last contributions. Fixing format ...

--- docs/d6.4/d6.4-33-betatesting.cmd
+++ docs/d6.4/d6.4-33-betatesting.cmd
@@ -1,6 +1,3 @@
 
-pushd C:\Projectos\reTHINK\WP7 Dissemination\git\hackathons\2017-May
 
-pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. evaluation.md -o C:\Projectos\reTHINK\WP6\git\testbeds\docs\d6.4\D6.4-33-betatesting.docx
-
-pushd C:\Projectos\reTHINK\WP6\git\testbeds\docs\d6.4
+pandoc --filter pandoc-citeproc -f markdown -t docx --data-dir=. https://raw.githubusercontent.com/reTHINK-project/hackathons/master/2017-May/evaluation.md -o D6.4-33-betatesting.docx
